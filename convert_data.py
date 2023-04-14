a512d1a01fb481d0d5f63fb5527f271237180790
bcoop <bcoop@live.unc.edu>
2023-04-14 01:20:16 -0400

    added sql code to create table for match_data

--- convert_data.py
+++ convert_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import json
 
 # Load the JSON data
-with open("epl_2022_2023_07_02_2023.json", "r") as file:
-    json_data = json.load()
+with open("epl_2022_2023_07_02_2023.json", "r", encoding = "utf-8") as file:
+    json_data = json.load(file)
 
 # Convert JSON data to a pandas DataFrame
 data_frame = pd.json_normalize(json_data)
